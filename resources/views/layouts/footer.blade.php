7bfc6e5810f1c65bdc247e0f8b3d2d3ce09b68d6
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2024-04-17 15:02:55 +0800

    nmis: Add moodLearning Copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
   <div class=" container-fluid ">
-    <nav>
+    {{-- <nav>
       <ul>
         <li>
           <a href="https://www.creative-tim.com" target="_blank">
@@ -22,15 +22,15 @@
             {{__(" Updivision")}}</a>
         </li>
       </ul>
-    </nav>
+    </nav> --}}
     <div class="copyright" id="copyright">
       &copy;
       <script>
         document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
       </script>, {{__(" Designed by")}}
-      <a href="https://www.invisionapp.com" target="_blank">{{__(" Invision")}}</a>{{__(" . Coded by")}}
-      <a href="https://www.creative-tim.com" target="_blank">{{__(" Creative Tim ")}}</a>&
-      <a href="https://www.updivision.com" target="_blank">{{__(" Updivision")}}</a>
+      <a href="https://www.moodlearning.com" target="_blank">{{__(" moodLearning")}}</a>
+      {{-- <a href="https://www.creative-tim.com" target="_blank">{{__(" Creative Tim ")}}</a>&
+      <a href="https://www.updivision.com" target="_blank">{{__(" Updivision")}}</a> --}}
     </div>
   </div>
 </footer>
