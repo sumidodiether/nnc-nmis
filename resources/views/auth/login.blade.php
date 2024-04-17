e9e447613356ec7642f8ac1a788bb8d6f2211a3e
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2024-04-17 15:01:19 +0800

    nmis: Add NNC Login UI Design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     'namePage' => 'Login page',
     'class' => 'login-page sidebar-mini ',
     'activePage' => 'login',
-    'backgroundImage' => asset('assets') . "/img/bg14.jpg",
+    'backgroundImage' => asset('assets') . "/img/background1.png",
 ])
 
 @section('content')
@@ -28,33 +28,33 @@
         <div class="col-md-4 ml-auto mr-auto">
             <form role="form" method="POST" action="{{ route('login') }}">
                 @csrf
-            <div class="card card-login card-plain">
+            <div class="card card-login card-plain bg-white border" style="border-radius:40px;">
                 <div class="card-header ">
-                <div class="logo-container">
-                    <img src="{{ asset('assets/img/now-logo.png') }}" alt="">
+                <div class="logo-container" style="width:150px; margin:2rem auto;">
+                    <img src="{{ asset('assets/img/logo.png') }}" alt="">
                 </div>
                 </div>
                 <div class="card-body ">
-                <div class="input-group no-border form-control-lg {{ $errors->has('email') ? ' has-danger' : '' }}">
+                <div class="input-group no-border form-control-lg {{ $errors->has('email') ? ' has-danger' : '' }}" style="border-radius:40px; background:#e6e6e6;">
                     <span class="input-group-prepend">
                     <div class="input-group-text">
-                        <i class="now-ui-icons users_circle-08"></i>
+                        <i class="now-ui-icons users_circle-08 text-dark"></i>
                     </div>
                     </span>
-                    <input class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" type="email" name="email" value="{{ old('email', 'admin@nowui.com') }}" required autofocus>
+                    <input class="form-control text-dark {{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" type="email" name="email" value="{{ old('email', 'Input Username') }}" required autofocus>
                 </div>
                 @if ($errors->has('email'))
                     <span class="invalid-feedback" style="display: block;" role="alert">
                     <strong>{{ $errors->first('email') }}</strong>
                     </span>
                 @endif
-                <div class="input-group no-border form-control-lg {{ $errors->has('password') ? ' has-danger' : '' }}">
+                <div class="input-group no-border form-control-lg {{ $errors->has('password') ? ' has-danger' : '' }}" style="border-radius:40px; background:#e6e6e6; margin-bottom:5px;">
                     <div class="input-group-prepend">
                     <div class="input-group-text">
-                        <i class="now-ui-icons objects_key-25"></i></i>
+                        <i class="now-ui-icons objects_key-25 text-dark"></i></i>
                     </div>
                     </div>
-                    <input placeholder="Password" class="form-control {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ __('Password') }}" type="password" value="secret" required>
+                    <input placeholder="Password" class="form-control text-dark {{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="{{ __('Password') }}" type="password" value="secret" required>
                 </div>
                 @if ($errors->has('password'))
                     <span class="invalid-feedback" style="display: block;" role="alert">
@@ -63,17 +63,17 @@
                 @endif
                 </div>
                 <div class="card-footer ">
+                    <div class="pull-left">
+                        <h6>
+                        <a href="{{ route('register') }}" class="link footer-link text-dark">{{ __('Create Account') }}</a>
+                        </h6>
+                    </div>
+                    <div class="pull-right">
+                        <h6>
+                        <a href="{{ route('password.request') }}" class="link footer-link text-dark">{{ __('Forgot Password?') }}</a>
+                        </h6>                
+                    </div>
                 <button  type = "submit" class="btn btn-primary btn-round btn-lg btn-block mb-3">{{ __('Get Started') }}</button>
-                <div class="pull-left">
-                    <h6>
-                    <a href="{{ route('register') }}" class="link footer-link">{{ __('Create Account') }}</a>
-                    </h6>
-                </div>
-                <div class="pull-right">
-                    <h6>
-                    <a href="{{ route('password.request') }}" class="link footer-link">{{ __('Forgot Password?') }}</a>
-                    </h6>                
-                </div>
                 </div>
             </div>
             </form>
